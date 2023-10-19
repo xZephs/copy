cf24e4a1a6167ae3c23b7613f7d449a0692e0aa9
arkanjumani <148118853+arkanjumani@users.noreply.github.com>
2023-10-19 19:44:39 -0400

    Deleted person.java due to error

--- sourcecode/Person.java
+++
@@ -1,4 +0,0 @@
-//Creator: Arkan Jumani 
-public interface Person { //person interface that is shared with the Applicant class 
-    public String getInfo(); //getInfo method displays information in one concise statement.
-}
