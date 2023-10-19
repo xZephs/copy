5a0efc615e4535221a7633cff60ccc63034afbe1
arkanjumani <148118853+arkanjumani@users.noreply.github.com>
2023-10-19 19:40:16 -0400

    Add files via upload

---
+++ sourcecode/Person.java
@@ -0,0 +1,4 @@
+//Creator: Arkan Jumani 
+public interface Person { //person interface that is shared with the Applicant class 
+    public String getInfo(); //getInfo method displays information in one concise statement.
+}
