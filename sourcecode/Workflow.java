c4ac2395e757e9d8b88db838f4ee20c0f17bc696
xZephs <117756075+xZephs@users.noreply.github.com>
2023-10-19 19:27:25 -0400

    Create Workflow.java
    Workflow class to create and manage the Workflow object. Transitioning data between components.

---
+++ sourcecode/Workflow.java
@@ -0,0 +1,68 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * This class represents a workflow object.
+ */
+public class Workflow{
+
+   Queue<String> steps;
+   boolean isCompleted;
+   Document doc;
+
+   /**
+    * Private constructor to create a Workflow object.
+    */
+   private Workflow(){
+      steps = new LinkedList<>();
+      steps.add("Data Entry");
+      steps.add("Review");
+      steps.add("Approval");
+      isCompleted = false;
+   }
+
+   public static Workflow createWorkflow(){
+    return new Workflow();
+
+   }
+
+   public Document getDoc(){
+      return this.doc;
+   }
+
+   public boolean isCompleted(){
+      return isCompleted;
+   }
+
+   /**
+    * This method is used to update in Data Entry and Review
+    * @param stepname
+    * @param d
+    * @return true if data is updated successfully, false otherwise.
+    */
+   public boolean updateData(String stepname, Document d){
+    return false;
+   }
+
+   /**
+    * This method is used to update in Approval
+    * @param stepname
+    * @param d
+    * @param flag
+    * @return true if data is updated successfully, false otherwise.
+    */
+   public boolean updateData(String stepname, Document d, boolean flag){
+    return false;
+   }
+
+   /**
+    * Show the current status of the workflow
+    * @return
+    */
+   public String showStatus(){
+    return "";
+   }
+
+   
+
+}
