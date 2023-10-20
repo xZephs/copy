70eeba51298f1b061d0aa06a226638c31e51383b
xZephs <117756075+xZephs@users.noreply.github.com>
2023-10-20 15:43:43 -0400

    Update DocumentTest.java

--- testcode/DocumentTest.java
+++ testcode/DocumentTest.java
@@ -15,10 +15,10 @@ public class DocumentTest {
         Immigrant i = Immigrant.createImmigrant("Andrew", "Braddock", 25, 12345);
         
         assertNull(Document.createDoc(null,null,-1,-1));       //All invalid valid inputs
-        assertNull(Document.createDoc(i,a,200,1));         //Invalid age input
-        assertNull(Document.createDoc(null,a,30,1));     //Invalid immigrant input
-        assertNull(Document.createDoc(i,null,30,1));     //Invalid applicant input
-        assertNull(Document.createDoc(i,a,30,-1000000));      //Invalid id input
+        assertNull(Document.createDoc(i,a,200,1));             //Invalid age input
+        assertNull(Document.createDoc(null,a,30,1));           //Invalid immigrant input
+        assertNull(Document.createDoc(i,null,30,1));           //Invalid applicant input
+        assertNull(Document.createDoc(i,a,30,-1000000));       //Invalid id input
 
         //Successfully create Document
         assertEquals(Document.createDoc(i, a, 23, 1), Document.class);
