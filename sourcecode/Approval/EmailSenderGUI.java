c969a8464fb2c02e316fb76343b770ede8af983e
xZephs <117756075+xZephs@users.noreply.github.com>
2023-11-16 21:41:31 -0500

    Add files via upload

---
+++ sourcecode/Approval/EmailSenderGUI.java
@@ -0,0 +1,50 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+/**
+ * A class to send email
+ * @author M. Phan
+ */
+public class EmailSenderGUI {
+    private JFrame frame;
+    private JTextArea emailContent;
+
+    public EmailSenderGUI() {
+        frame = new JFrame("Email Sender");
+        frame.setSize(400, 300);
+
+        emailContent = new JTextArea();
+        emailContent.setLineWrap(true);
+        emailContent.setWrapStyleWord(true);
+
+        JButton sendButton = new JButton("Send");
+
+        sendButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent evt) {
+                // Simulate sending email 
+                boolean emailSent = true;
+
+                if (emailSent) {
+                    JOptionPane.showMessageDialog(frame, "Email sent successfully!");
+                    openMainMenu(); // Open the main menu after message is shown
+                }
+            }
+        });
+
+        frame.add(new JScrollPane(emailContent), BorderLayout.CENTER);
+        frame.add(sendButton, BorderLayout.SOUTH);
+
+        frame.setVisible(true);
+    }
+
+    private void openMainMenu() {
+        frame.dispose(); // Close the current window
+
+        // Create and show the main menu window
+        MainMenu mainMenu = new MainMenu();
+        mainMenu.showMainMenu();
+    }
+
+   
+}
