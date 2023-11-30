b817fca325df4a4ad0571838a117e03268534c18
xZephs <117756075+xZephs@users.noreply.github.com>
2023-11-30 13:16:59 -0500

    Add files via upload

---
+++ src/test/java/DocumentTest.java
@@ -0,0 +1,46 @@
+import static org.hamcrest.CoreMatchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.jupiter.api.Test;
+
+public class DocumentTest {
+
+    Applicant a = Applicant.createApplicant("Andrew", "Braddock", 25, 12345, "@gmail");
+    Immigrant i = Immigrant.createImmigrant("Andrew", "Braddock", 25, 12345);
+    
+    @Test
+    void testAllInvalid(){
+        assertNull(Document.createDoc(null,null,-1,-1));       //All invalid valid inputs
+    }
+
+    @Test
+    void testNullApplicant(){
+        assertNull(Document.createDoc(i,null,30,1));     //Invalid applicant input
+    }
+
+    @Test 
+    void testNullImmigrant(){
+        assertNull(Document.createDoc(null,a,30,1));     //Invalid immigrant input
+    }
+
+    @Test 
+    void testInvalidForm(){
+        assertNull(Document.createDoc(i,a,-1,1));        //Invalid form
+    }
+
+    @Test
+    void testInvalidID(){
+        assertNull(Document.createDoc(i,a,30,-1000000));      //Invalid id input
+    }
+
+    @Test 
+    void testSuccess(){
+        //Successfully create Document
+        assertEquals(Document.createDoc(i, a, 23, 1), Document.class);
+        Document d = Document.createDoc(i, a, 23, 1);
+        assertNotNull(d);
+        assertEquals(d.getApplicant(), a);
+    }
+
+   
+}
