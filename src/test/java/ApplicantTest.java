36fd52a809d2d8fac44ba3f9341059fc5ede9458
xZephs <117756075+xZephs@users.noreply.github.com>
2023-11-29 20:51:32 -0500

    Update ApplicantTest.java

--- src/test/java/ApplicantTest.java
+++ src/test/java/ApplicantTest.java
@@ -24,7 +24,7 @@ class ApplicantTest{
     assertEquals("47925 Silly Lane", temp.getAddress());
     assertEquals("12345@gmail.com", temp.getEmail());
     assertTrue(temp.getAge()==37);
-    assertTrue(temp.getID() == 475278);
+    assertTrue(temp.getRealID() == 475278);
   }
   @Test
   void instantiateInvalidInputTest()
